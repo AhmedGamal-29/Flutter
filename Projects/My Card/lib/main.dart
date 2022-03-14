49871494a8cfb540f2fc5cabf30879c9d3e51425
Ahmed Gamal <101002059+AhmedGamal-29@users.noreply.github.com>
2022-03-14 23:25:23 +0200

    Add files via upload

--- Projects/My Card/lib/main.dart
+++ Projects/My Card/lib/main.dart
@@ -1,1 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/gemi.jpg'),
+              ),
+              Text(
+                'Ahmed Gamal',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text('FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                  )),
+              SizedBox3(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '01149418514',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0),
+                    ),
+                  )),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'ahmed291.ag@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
